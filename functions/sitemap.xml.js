a4ef41a1309132481ddafc5c426dbc84172c361c
shogunnx <99447559+shogunnx@users.noreply.github.com>
2025-10-02 15:11:13 -0500

    Update sitemap.xml.js

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -1,13 +1,19 @@
 export async function onRequest() {
   const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://wiki.thesaiyanvictoria.com/</loc>
-  </url>
-  <url>
-    <loc>https://wiki.thesaiyanvictoria.com/wiki/Main_Page</loc>
-  </url>
-  <!-- Add more <url><loc>…</loc></url> lines for your important wiki pages -->
+  <url><loc>https://wiki.thesaiyanvictoria.com/</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Main_Page</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Victoria_Black</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Wargirl</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Binary</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Vanessa</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Harmony</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Evil_Victoria</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Gas_Mask_Clan</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Merged_Zamasu</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Broly_Raid</loc></url>
+  <url><loc>https://wiki.thesaiyanvictoria.com/wiki/Rescue_of_Wargirl</loc></url>
+  <!-- Add more links below as needed -->
 </urlset>`;
   return new Response(body, {
     headers: {
